de99f83e9e0e86a14e10669e8483eb1358bd0858
Cristian Marquez <xthecapx@gmail.com>
2019-01-22 15:05:53 -0500

    Added component schema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TimerComponent } from './timer/timer.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './parent/child/child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule
